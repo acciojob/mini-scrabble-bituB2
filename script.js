67c4bb49358673149a9454e62595caf6c4916fd2
Bitu Abraham <120098241+bituB2@users.noreply.github.com>
2023-02-11 07:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+const inputText = document.getElementById("evaluatedText");
+const letterCountDisplay = document.getElementById("letterCount");
+
+inputText.addEventListener("input", function() {
+  let letterCount = this.value.length;
+  if (letterCount === 0) {
+    letterCountDisplay.innerHTML = 0;
+  } else {
+    letterCountDisplay.innerHTML = letterCount;
+  }
+});
+
